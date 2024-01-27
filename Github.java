4364d3caa022973abba36b1f571a22ddd2392952
OwenAtIreton <ojpenrose27@bishopireton.org>
2024-01-27 11:42:21 -0500

    Added diologue

--- Github.java
+++ Github.java
@@ -4,10 +4,17 @@ This is a java file
 Its on github now
 Isnt that cool
 */
-
+import java.util.Scanner;
 class Github {
    public static void main(String[] args){
-   
+      Scanner kb = new Scanner(System.in);
       System.out.print("Wow! github!");
+      System.out.println("Do you like github (Y/N)");
+      String response = kb.next();
+      if (response.equalsIgnoreCase("y")) {
+         System.out.print("Great! me too!");
+      } else {
+         System.out.print("Oh... Thats a shame.");
+      }
    }
 }
