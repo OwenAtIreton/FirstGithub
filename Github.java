d3e03e80a9608820565c3e469b9f0a5c2ba9a46e
OwenAtIreton <ojpenrose27@bishopireton.org>
2024-01-27 11:35:38 -0500

    new

---
+++ Github.java
@@ -0,0 +1,13 @@
+/*
+Hello
+This is a java file
+Its on github now
+Isnt that cool
+*/
+
+class Github {
+   public static void main(String[] args){
+   
+      System.out.print("Wow! github!");
+   }
+}
